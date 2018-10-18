cdfa7217100cbace9a36afe65138b2ef9e17ab69
hayeksmith <42807352+hayeksmith@users.noreply.github.com>
2018-10-18 20:53:03 +0200

    Update townhalls_adder_to_db.rb
    adder

---
+++ lib/app/townhalls_adder_to_db.rb
@@ -0,0 +1,17 @@
+require 'json'
+require 'csv'
+
+require_relative 'townhalls_follower.rb'
+#pas finis a ararnger
+class TwitterToJson
+
+    def initialize(array_returned)
+        @a = File.open("../../db/all_emails.JSON","w") 
+        @a.write(TweeTown.tweeter_account.to_json)
+
+    
+
+    def add_more(array)
+        @a.write(array.to_json)
+    end
+end`
