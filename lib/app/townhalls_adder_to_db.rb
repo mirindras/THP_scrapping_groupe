5e9151f86dd001623c774de09db7723f50aba728
Stephan Dev <stephan-dev@outlook.fr>
2018-10-18 23:04:00 +0200

    fermé méthode

--- lib/app/townhalls_adder_to_db.rb
+++ lib/app/townhalls_adder_to_db.rb
@@ -9,7 +9,7 @@ class TwitterToJson
         @a = File.open("../../db/all_emails.JSON","w") 
         @a.write(TweeTown.tweeter_account.to_json)
 
-    
+    end
 
     def add_more(array)
         @a.write(array.to_json)
