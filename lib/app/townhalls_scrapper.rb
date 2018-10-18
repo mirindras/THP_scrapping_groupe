ffd12db49ba1d9da3cf21e4ef977c20023a19222
stephan-dev <stephan-dev@outlook.fr>
2018-10-18 14:38:47 +0200

    scraper 9 pages

--- lib/app/townhalls_scrapper.rb
+++ lib/app/townhalls_scrapper.rb
@@ -12,25 +12,68 @@ class Scrapper
     doc.css('td')[7].text 
   end
 
-  def get_all_the_urls_of_val_doise_townhalls(url)
-    doc = Nokogiri::HTML(open(url))
-    links = doc.css("a[class=lientxt]")
+  def get_all_the_urls_of_departement_townhalls(urldepartementx)
+    doc = Nokogiri::HTML(open(urldepartementx))
+    endlinks = doc.css("a[class=lientxt]")
 
-    array_final = Array.new
+    endlinks.each do |endlink|    
+      villemail = Array.new 
+      #ville += urldepartementx.text      
 
-    urls = links.each {|url|    
-      hash_ville_mail = Hash.new
-      hash_ville_mail['ville'] = url.text
-      hash_ville_mail ['email'] = get_the_email_of_a_townhal_from_its_webpage("http://annuaire-des-mairies.com/"+url['href'][1..-1])
-      array_final.push(hash_ville_mail)
-      }
-      print array_final
-      File.open("db/all_emails.JSON","w") do |f|
-      f.write(array_final.to_json)
+      email = get_the_email_of_a_townhal_from_its_webpage("http://annuaire-des-mairies.com/"+endlink['href'][1..-1])
+
+      emails = Array.new
+      emails.push(email)
+
+      #puts emails
+
+      #puts endlink['href'][5..-6]
+
+      ville = Array.new
+      ville.push(endlink['href'][5..-6])
+
+      hashvillemail = Hash.new
+
+      hashvillemail = {
+        "ville" => ville,
+        "email" => emails,
+        "département" => urldepartementx[32..-6],
+        }
+      puts hashvillemail
+     
+      # File.open("../../db/all_emails.JSON","w") do |f|
+      # f.write(array_final.to_json)
       end
+
+      puts 
+      puts
+      puts
+      puts
+      puts
+      puts "fin département"
   end 
 
+  def add_to_array
+    array_final = Array.new
+    array_final.push(hash_ville_mail)
+  end
+
   def perform
-    get_all_the_urls_of_val_doise_townhalls("http://annuaire-des-mairies.com/calvados.html")
+    get_all_the_urls_of_departement_townhalls("http://annuaire-des-mairies.com/calvados.html")
+    get_all_the_urls_of_departement_townhalls("http://annuaire-des-mairies.com/calvados-2.html")
+    get_all_the_urls_of_departement_townhalls("http://annuaire-des-mairies.com/calvados-3.html")
+    get_all_the_urls_of_departement_townhalls("http://annuaire-des-mairies.com/calvados-4.html")
+
+    get_all_the_urls_of_departement_townhalls("http://annuaire-des-mairies.com/manche.html")
+    get_all_the_urls_of_departement_townhalls("http://annuaire-des-mairies.com/manche-2.html")
+    get_all_the_urls_of_departement_townhalls("http://annuaire-des-mairies.com/manche-3.html")
+
+    get_all_the_urls_of_departement_townhalls("http://annuaire-des-mairies.com/ille-et-vilaine.html")
+    get_all_the_urls_of_departement_townhalls("http://annuaire-des-mairies.com/ille-et-vilaine-2.html")
+
+    get_all_the_urls_of_departements_townhalls("http://annuaire-des-mairies.com/ileetvilaine.html")
   end
 end
+
+truc = Scrapper.new
+truc.perform
