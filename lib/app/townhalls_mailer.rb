a98b47257859429a9f016b9a38be1ee2c5b0b0e8
Arnaud Jeannin <Arnaud@mbp-de-arnaud.lan>
2018-10-18 18:04:01 +0200

    Emailing OK, code à clarifier

--- lib/app/townhalls_mailer.rb
+++ lib/app/townhalls_mailer.rb
@@ -2,13 +2,17 @@ require 'google/apis/gmail_v1'
 require 'googleauth'
 require 'googleauth/stores/file_token_store'
 require 'fileutils'
+require 'json'
+
+emails_json = JSON.parse(File.read "../../db/all_emails.JSON")
 
 OOB_URI = 'urn:ietf:wg:oauth:2.0:oob'.freeze
-APPLICATION_NAME = 'Mairies THP'.freeze
+APPLICATION_NAME = 'THP Mairies'.freeze
 CREDENTIALS_PATH = 'credentials.json'.freeze
 TOKEN_PATH = 'token.yaml'.freeze
-SCOPE = Google::Apis::GmailV1::AUTH_GMAIL_READONLY
+SCOPE = Google::Apis::GmailV1::AUTH_SCOPE
 
+##
 # Ensure valid credentials, either by restoring from the saved credentials
 # files or intitiating an OAuth2 authorization. If authorization is required,
 # the user's default browser will be launched to approve the request.
@@ -37,9 +41,15 @@ service = Google::Apis::GmailV1::GmailService.new
 service.client_options.application_name = APPLICATION_NAME
 service.authorization = authorize
 
-# Show the user's labels
-user_id = 'me'
-result = service.list_user_labels(user_id)
-puts 'Labels:'
-puts 'No labels found' if result.labels.empty?
-result.labels.each { |label| puts "- #{label.name}" }
+emails_json.each do |i| 
+message = Google::Apis::GmailV1::Message.new(:raw => "To: #{i["email"]}\r\nSubject: The Hacking Project x #{i["ville"]}: 3 mois pour apprendre à coder\r\n\r\nBonjour,
+Je m'appelle Igor, je suis élève à The Hacking Project, une formation au code gratuite, sans locaux, sans sélection, sans restriction géographique. La pédagogie de ntore école est celle du peer-learning, où nous travaillons par petits groupes sur des projets concrets qui font apprendre le code. Le projet du jour est d'envoyer (avec du codage) des emails aux mairies pour qu'ils nous aident à faire de The Hacking Project un nouveau format d'éducation pour tous.
+
+Déjà 500 personnes sont passées par The Hacking Project. Est-ce que la mairie de #{i["ville"]} veut changer le monde avec nous ?
+
+
+Charles, co-fondateur de The Hacking Project pourra répondre à toutes vos questions : 06.95.46.60.80")
+service.send_user_message('me', message)
+end
+
+
